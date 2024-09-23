e1f67e7b21766bab590b9619710b3e436e22bbcd
Admin <14389483+shenyin-ale_0@user.noreply.gitee.com>
2024-09-23 17:37:37 +0800

    jc-club-subject

---
+++ jc-club-subject/jc-club-domain/target/generated-sources/annotations/com/jingdianjichi/subject/domain/convert/JudgeSubjectConverterImpl.java
@@ -0,0 +1,41 @@
+package com.jingdianjichi.subject.domain.convert;
+
+import com.jingdianjichi.subject.domain.entity.SubjectAnswerBO;
+import com.jingdianjichi.subject.infra.basic.entity.SubjectJudge;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-09-20T17:57:16+0800",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.22 (Amazon.com Inc.)"
+)
+public class JudgeSubjectConverterImpl implements JudgeSubjectConverter {
+
+    @Override
+    public List<SubjectAnswerBO> convertEntityToBoList(List<SubjectJudge> subjectJudgeList) {
+        if ( subjectJudgeList == null ) {
+            return null;
+        }
+
+        List<SubjectAnswerBO> list = new ArrayList<SubjectAnswerBO>( subjectJudgeList.size() );
+        for ( SubjectJudge subjectJudge : subjectJudgeList ) {
+            list.add( subjectJudgeToSubjectAnswerBO( subjectJudge ) );
+        }
+
+        return list;
+    }
+
+    protected SubjectAnswerBO subjectJudgeToSubjectAnswerBO(SubjectJudge subjectJudge) {
+        if ( subjectJudge == null ) {
+            return null;
+        }
+
+        SubjectAnswerBO subjectAnswerBO = new SubjectAnswerBO();
+
+        subjectAnswerBO.setIsCorrect( subjectJudge.getIsCorrect() );
+
+        return subjectAnswerBO;
+    }
+}
---
+++ jc-club-subject/jc-club-domain/target/generated-sources/annotations/com/jingdianjichi/subject/domain/convert/SubjectInfoConverterImpl.java
@@ -0,0 +1,66 @@
+package com.jingdianjichi.subject.domain.convert;
+
+import com.jingdianjichi.subject.domain.entity.SubjectInfoBO;
+import com.jingdianjichi.subject.infra.basic.entity.SubjectInfo;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-09-20T17:57:16+0800",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.22 (Amazon.com Inc.)"
+)
+public class SubjectInfoConverterImpl implements SubjectInfoConverter {
+
+    @Override
+    public SubjectInfo convertBoToInfo(SubjectInfoBO subjectInfoBO) {
+        if ( subjectInfoBO == null ) {
+            return null;
+        }
+
+        SubjectInfo subjectInfo = new SubjectInfo();
+
+        subjectInfo.setId( subjectInfoBO.getId() );
+        subjectInfo.setSubjectName( subjectInfoBO.getSubjectName() );
+        subjectInfo.setSubjectDifficult( subjectInfoBO.getSubjectDifficult() );
+        subjectInfo.setSettleName( subjectInfoBO.getSettleName() );
+        subjectInfo.setSubjectType( subjectInfoBO.getSubjectType() );
+        subjectInfo.setSubjectScore( subjectInfoBO.getSubjectScore() );
+        subjectInfo.setSubjectParse( subjectInfoBO.getSubjectParse() );
+
+        return subjectInfo;
+    }
+
+    @Override
+    public List<SubjectInfoBO> convertListInfoToBO(List<SubjectInfo> subjectInfoList) {
+        if ( subjectInfoList == null ) {
+            return null;
+        }
+
+        List<SubjectInfoBO> list = new ArrayList<SubjectInfoBO>( subjectInfoList.size() );
+        for ( SubjectInfo subjectInfo : subjectInfoList ) {
+            list.add( subjectInfoToSubjectInfoBO( subjectInfo ) );
+        }
+
+        return list;
+    }
+
+    protected SubjectInfoBO subjectInfoToSubjectInfoBO(SubjectInfo subjectInfo) {
+        if ( subjectInfo == null ) {
+            return null;
+        }
+
+        SubjectInfoBO subjectInfoBO = new SubjectInfoBO();
+
+        subjectInfoBO.setId( subjectInfo.getId() );
+        subjectInfoBO.setSubjectName( subjectInfo.getSubjectName() );
+        subjectInfoBO.setSubjectDifficult( subjectInfo.getSubjectDifficult() );
+        subjectInfoBO.setSettleName( subjectInfo.getSettleName() );
+        subjectInfoBO.setSubjectType( subjectInfo.getSubjectType() );
+        subjectInfoBO.setSubjectScore( subjectInfo.getSubjectScore() );
+        subjectInfoBO.setSubjectParse( subjectInfo.getSubjectParse() );
+
+        return subjectInfoBO;
+    }
+}
